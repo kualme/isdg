435e5cb9f3a9f27685916288e28433dafa1afa2f
Nataly Munts <natalymunts@gmail.com>
2016-05-02 14:02:14 +0500

    Design
    Admin theme was used

--- Isdg/App_Start/BundleConfig.cs
+++ Isdg/App_Start/BundleConfig.cs
@@ -26,11 +26,12 @@ namespace Isdg
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap.js",                      
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/sb-admin.css",                      
                       "~/Content/site.css"));
         }
     }
